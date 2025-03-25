a57427998dc24f399229f6b7891f317c96dbc178
Yadiel Valentin Rios <yadielvalentin1389@gmail.com>
2025-03-25 00:28:49 -0500

    3/25 12:28
    Added stacks for the history and notation, and pop and push functions to access them.

--- Project1.rkt
+++ Project1.rkt
@@ -1,3 +1,15 @@
 #lang racket
 
 (displayln "Enter a postfix/Polish notation expression:")
+
+(define history null)
+
+(define notation null)
+
+(define (pop stack) ; usable for either history or notation
+  (if (null? stack)
+     (error "The stack you are trying to access is empty")
+     (values (car stack) (cdr stack))))
+
+(define (push stack) ; also applicable for both locations
+  (cons item stack))
