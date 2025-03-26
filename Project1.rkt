0bc7a287f865eb05b0de5c659c9148774c740965
Yadiel Valentin Rios <yadielvalentin1389@gmail.com>
2025-03-26 15:30:42 -0500

    3/26 3:29
    Whoops! I just realized that I was doing it in postfix when I should have been doing it in prefix. I've changed some of the commands to now wait for when there are 2 numbers in the stack and then pop the top operation. It turns out I needed an operation stack after all

--- Project1.rkt
+++ Project1.rkt
@@ -6,6 +6,8 @@
 
 (define nums null)
 
+(define operations null)
+
 (define (pop stack)
   (if (null? stack)
       (error "The stack you are trying to access is empty") 
@@ -17,24 +19,31 @@
 ; Calculator
 
 (define (get-notation)
-  (display "Enter a postfix/Polish notation expression (or type \"quit\" to quit: ")
+  (display "Enter a prefix expression (or type \"quit\" to quit): ")
   (define input (read-line))
   (unless (equal? input "quit")
     (for ([char (string->list input)])
       (cond
         ; if is a number
         [(char-numeric? char)
-         (set! nums (push nums (string->number (string char))))]
+         (set! nums (push nums (string->number (string char))))
+         (when (= (length nums) 2)
+           (let-values ([(num1 new-nums) (pop nums)])
+             (let-values ([(num2 final-nums) (pop new-nums)])
+               (define result (calculate num1 num2 (pop operations)))
+               (set! nums (push final-nums result)))))]
+
+
         ; if is an operation
         [(member char '(#\+ #\- #\* #\/))
-         (let-values ([(num1 new-nums) (pop nums)])
-           (let-values ([(num2 final-nums) (pop new-nums)])
-             (define result (calculate num1 num2 char))
-             (set! nums (push final-nums result))))]
+         (set! operations (push operations char))]
+
+        
+        [(member char '(#\ ))]
         [else (displayln("Invalid input detected. Must be either a number or a simple operation ('+', '-', '*', '/')."))]))
-  (displayln nums) ; if done correctly, nums should only have the final answer at the end
-  (set! nums null)
-  (get-notation)))
+    (displayln nums) ; if done correctly, nums should only have the final answer at the end
+    (set! nums null)
+    (get-notation)))
 
 (define (calculate num1 num2 operation)
   (define result
@@ -44,7 +53,7 @@
       [(char=? operation #\*) (* num1 num2)]
       [(char=? operation #\/) (if (= num2 0)
                                   (error "Division by 0 is not allowed!")
-                                  (/ num1 num2))]
+                                  (/ num2 num1))]
       [else (error "Invalid operators! Only '+', '-', '*', '/' are allowed!")]))
   (set! history (push history result))
   result)
@@ -52,3 +61,10 @@
 ; Run all together
 
 (get-notation)
+
+
+
+;(let-values ([(num1 new-nums) (pop nums)])
+;          (let-values ([(num2 final-nums) (pop new-nums)])
+;           (define result (calculate num1 num2 char))
+;          (set! nums (push final-nums result))))
