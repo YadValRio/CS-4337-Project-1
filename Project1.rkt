38152d50fc95bff26c087dea1dc9a92de56228dc
Yadiel Valentin Rios <yadielvalentin1389@gmail.com>
2025-03-26 17:28:45 -0500

    3/26 5:28
    Updated outputs to fit the rubric

--- Project1.rkt
+++ Project1.rkt
@@ -8,7 +8,7 @@
 
 (define (pop stack)
   (if (null? stack)
-      (error "The stack you are trying to access is empty") 
+      (error "Error: the stack you are trying to access is empty") 
       (values (car stack) (cdr stack))))
 
 (define (push stack item)
@@ -31,7 +31,7 @@
            (let-values ([(num2 final-nums) (pop new-nums)])
              (define result (calculate num1 num2 char))
              (set! nums (push final-nums result))))]
-        [else (displayln("Invalid input detected. Must be either a number or a simple operation ('+', '-', '*', '/')."))]))
+        [else (displayln("Error: invalid input detected. Must be either a number or a simple operation ('+', '-', '*', '/')."))]))
   (displayln nums) ; if done correctly, nums should only have the final answer at the end
   (set! nums null)
   (get-notation)))
@@ -43,9 +43,9 @@
       [(char=? operation #\-) (- num1 num2)]
       [(char=? operation #\*) (* num1 num2)]
       [(char=? operation #\/) (if (= num2 0)
-                                  (error "Division by 0 is not allowed!")
+                                  (error "Error: division by 0 is not allowed!")
                                   (/ num1 num2))]
-      [else (error "Invalid operators! Only '+', '-', '*', '/' are allowed!")]))
+      [else (error "Error: invalid operators! Only '+', '-', '*', '/' are allowed!")]))
   (set! history (push history result))
   result)
 
